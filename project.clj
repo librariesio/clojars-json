b8a7abd3485bb12cfd383152c2e58cd9faf19dcf
Andrew Nesbitt <andrewnez@gmail.com>
2017-04-24 10:17:01 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,12 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/core.memoize "0.5.8"]
-                 [cheshire "5.4.0"]
-                 [compojure "1.3.2"]
-                 [ring/ring-defaults "0.1.2"]
-                 [ring/ring-jetty-adapter "1.2.1"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/core.memoize "0.5.9"]
+                 [cheshire "5.7.1"]
+                 [compojure "1.5.2"]
+                 [ring/ring-defaults "0.2.3"]
+                 [ring/ring-jetty-adapter "1.5.1"]]
   :plugins [[lein-ring "0.8.13"]]
   :min-lein-version "2.0.0"
   :ring {:handler clojars-json.core/app}
